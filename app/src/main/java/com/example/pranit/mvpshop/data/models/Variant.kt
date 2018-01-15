55b001713816a46bad3d1d88f844f325f55602d3
gru001 <pranitsawant01@gmail.com>
2018-01-15 19:29:12 +0530

    added relational db for varient

--- app/src/main/java/com/example/pranit/mvpshop/data/models/Variant.kt
+++ app/src/main/java/com/example/pranit/mvpshop/data/models/Variant.kt
@@ -1,24 +1,37 @@
 package com.example.pranit.mvpshop.data.models
 
+import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.PrimaryKey
 import com.example.pranit.mvpshop.TABLE_VARIANT
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-@Entity(tableName = TABLE_VARIANT)
+@Entity(tableName = TABLE_VARIANT, foreignKeys = arrayOf(ForeignKey(entity = Product::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("productId"),
+        onDelete = ForeignKey.CASCADE)))
 class Variant {
 
+    @PrimaryKey
     @SerializedName("id")
     @Expose
     var id: Int? = null
+    @ColumnInfo
     @SerializedName("color")
     @Expose
     var color: String? = null
+    @ColumnInfo
     @SerializedName("size")
     @Expose
     var size: Any? = null
+    @ColumnInfo
     @SerializedName("price")
     @Expose
     var price: Int? = null
+
+    @ColumnInfo
+    var productId: Int? = null
 
 }
