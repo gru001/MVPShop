85eb1e8a6d65753069a995707d4e3de47c4c6b9d
pranit <pranitsawant01@gmail.com>
2018-06-14 18:16:15 +0530

    updated local datasource, added activity to manifest

--- app/src/main/java/com/example/pranit/mvpshop/data/product/ProductLocalDataSource.kt
+++ app/src/main/java/com/example/pranit/mvpshop/data/product/ProductLocalDataSource.kt
@@ -1,12 +1,17 @@
 package com.example.pranit.mvpshop.data.product
 
+import android.util.Log
 import com.example.pranit.mvpshop.data.local.ProductDao
 import com.example.pranit.mvpshop.data.local.ShopLocalDataSource
 import com.example.pranit.mvpshop.util.AppExecutors
 
 class ProductLocalDataSource private constructor(val appExecutors: AppExecutors, val productDao: ProductDao) {
 
-
+    fun getProductByCategory(categoryId: Int) {
+        appExecutors.diskIO.execute{
+            Log.i(ProductLocalDataSource::class.java.name, "$productDao.getProductsByCateogryId(categoryId)")
+        }
+    }
 
     companion object {
         private var INSTANCE : ProductLocalDataSource? = null
--- app/src/main/java/com/example/pranit/mvpshop/product/ProductPresenter.kt
+++ app/src/main/java/com/example/pranit/mvpshop/product/ProductPresenter.kt
@@ -1,12 +1,18 @@
 package com.example.pranit.mvpshop.product
 
-class ProductPresenter(val productPresenter: ProductPresenter, val productView: ProductContract.ProductView) : ProductContract.Presenter{
+import com.example.pranit.mvpshop.data.product.ProductLocalDataSource
+
+class ProductPresenter(val dataSource: ProductLocalDataSource, val productView: ProductContract.ProductView) : ProductContract.Presenter{
 
     init {
         productView.presenter = this
     }
 
     override fun start() {
+        loadProducts()
+    }
 
+    fun loadProducts() {
+        dataSource.getProductByCategory(1)
     }
 }
